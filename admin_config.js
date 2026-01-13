7a31f79f52ddca3f579d9da17618278376740d75
wondo87 <doyeonwon87@gmail.com>
2026-01-14 03:35:56 +0900

    Update CUSTOMER_SYNC_URL with A/S auto-email features

--- admin_config.js
+++ admin_config.js
@@ -16,7 +16,7 @@ const DELETE_PASSWORD = '6454';
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlUqGjCjdVMgGKckrcrnnqrAc92ejl0DsZwNtSd5mcOK8VxGS5PZB5BvMyzVxkhb04Cg/exec';
 
 // [중요] 고객관리 동기화 전용 URL (integrated_apps_script.js 배포 URL)
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycby9ohA0LOXmgwNiE9NWjCn16XktM85hl6wZkPTZe1rPnkHWcV6ecppUIcOl9ke5iaeG/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbw2-VugXCzpYMSNKseiPdns0BOyOuetNwQGMfhJPojIYHKmtn5xeUclCn3jj8DQnTEKsg/exec';
 
 // 초기 설정값
 const DEFAULT_PROFIT_RATE = 15; // 기본 이윤율 (%)
