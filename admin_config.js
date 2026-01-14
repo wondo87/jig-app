cfe5fe2eda3d786c971117a82606fe59bfbe1694
wondo87 <doyeonwon87@gmail.com>
2026-01-14 12:28:02 +0900

    Update GAS URL with cleanupAsSheet function

--- admin_config.js
+++ admin_config.js
@@ -16,7 +16,7 @@ const DELETE_PASSWORD = '6454';
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlUqGjCjdVMgGKckrcrnnqrAc92ejl0DsZwNtSd5mcOK8VxGS5PZB5BvMyzVxkhb04Cg/exec';
 
 // [중요] 고객관리 동기화 전용 URL (integrated_apps_script.js 배포 URL)
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyk0GoXYlsutcmOqqSyGw8CTdYh8TYmQQDrJUp-KOpdqTdVdt1WVWO8diFRQE9tWXAShQ/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyQUS_3ybAWfst2pifUejiaONrL72P7o0CMRl7A6sukALui3wPostWx7fSP-NSsrEqYrw/exec';
 
 // 초기 설정값
 const DEFAULT_PROFIT_RATE = 15; // 기본 이윤율 (%)
