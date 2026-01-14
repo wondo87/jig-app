c848dd0e5ee417d4411198a42d94c76056f5ad26
wondo87 <doyeonwon87@gmail.com>
2026-01-14 13:23:57 +0900

    Update GAS URL with 견적서 trigger change

--- admin_config.js
+++ admin_config.js
@@ -16,7 +16,7 @@ const DELETE_PASSWORD = '6454';
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlUqGjCjdVMgGKckrcrnnqrAc92ejl0DsZwNtSd5mcOK8VxGS5PZB5BvMyzVxkhb04Cg/exec';
 
 // [중요] 고객관리 동기화 전용 URL (integrated_apps_script.js 배포 URL)
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbw5iDQQdvhxUiROHDwYm6WdAIkFlZwXy0ldcaBwrsRfpGHn0OcS7tdajH7hSLKjGWGpSQ/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbw1HAgkKAnTPOuQxzy6-zHYLlWnUYDGBsBD7V0Dkytg7lsDXtyaA8ieGbdl2ISEOUrtzA/exec';
 
 // 초기 설정값
 const DEFAULT_PROFIT_RATE = 15; // 기본 이윤율 (%)
