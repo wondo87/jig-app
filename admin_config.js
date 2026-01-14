a380ea47e8af608e91b63476d8427788cdb14a93
wondo87 <doyeonwon87@gmail.com>
2026-01-14 14:06:20 +0900

    Update GAS URL with initialized triggers and duplicate ID fixes

--- admin_config.js
+++ admin_config.js
@@ -16,7 +16,7 @@ const DELETE_PASSWORD = '6454';
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlUqGjCjdVMgGKckrcrnnqrAc92ejl0DsZwNtSd5mcOK8VxGS5PZB5BvMyzVxkhb04Cg/exec';
 
 // [중요] 고객관리 동기화 전용 URL (integrated_apps_script.js 배포 URL)
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbx26zviODJeJMv1AiGqHKn0KNuApkfdqZCQOp-q0-67PZPAhCXxTBlzrSC8beVNWn-xtA/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbz-35TzkbXKJwTjR_Wjy_NZn72L0eelGrwAs4CXeSMUfWZx6JGNW4FymvWtjTzdaaI4tg/exec';
 
 // 초기 설정값
 const DEFAULT_PROFIT_RATE = 15; // 기본 이윤율 (%)
