1e4f08a142945a218003c96b3f9e4fd680f8581b
wondo87 <doyeonwon87@gmail.com>
2026-01-16 09:52:39 +0900

    chore: update apps script web app url

--- admin_config.js
+++ admin_config.js
@@ -13,10 +13,10 @@ const MAIN_ADMIN = {
 const DELETE_PASSWORD_HASH = '80409fb2145a39539cf9c876c11c903d341c6166edce59e0e852604566cbb848';
 
 // Google Apps Script Web App URL (고객 데이터 동기화용)
-const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxfMpu1zV1PcotsZiUz5nHneuj6_fOPjogtegUltJkMULjuWwpnsAJpVPGIosmRkgl1GA/exec';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbz22YM6GaDDZbkStCa1UbIC1a2bbUIAEwf5WrMoaRgrMm4EScRfsEkMHotbnIJ7016deg/exec';
 
 // [중요] 고객관리 동기화 전용 URL (integrated_apps_script.js 배포 URL)
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbxfMpu1zV1PcotsZiUz5nHneuj6_fOPjogtegUltJkMULjuWwpnsAJpVPGIosmRkgl1GA/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbz22YM6GaDDZbkStCa1UbIC1a2bbUIAEwf5WrMoaRgrMm4EScRfsEkMHotbnIJ7016deg/exec';
 
 // 초기 설정값
 const DEFAULT_PROFIT_RATE = 15; // 기본 이윤율 (%)
