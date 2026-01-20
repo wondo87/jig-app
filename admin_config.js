b4e5de137bb53a0204e41a2bcad0866acc81c774
wondo87 <doyeonwon87@gmail.com>
2026-01-21 00:50:01 +0900

    Apps Script URL 업데이트

--- admin_config.js
+++ admin_config.js
@@ -25,8 +25,8 @@ const MAIN_ADMIN = MAIN_ADMINS[0];
 const DELETE_PASSWORD_HASH = '80409fb2145a39539cf9c876c11c903d341c6166edce59e0e852604566cbb848';
 
 // [통합] 고객관리/상담관리/원가관리 등 모든 데이터 동기화를 위한 Apps Script Web App URL (배포 후 생성된 URL)
-const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxht8kBdtEOlPf7lFcZRLHgm2B9IF0E1nbCfy2-gl6VS4dAhfkhhKeXWT4B1QoIOZY3Sg/exec';
-const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbxht8kBdtEOlPf7lFcZRLHgm2B9IF0E1nbCfy2-gl6VS4dAhfkhhKeXWT4B1QoIOZY3Sg/exec';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxg8K7Kc_rPuBJsLebkMpjkhmm2ePiYKVJsLbT85r9hAQQY2NCahH8jaSTRZzKvR-KLSA/exec';
+const CUSTOMER_SYNC_URL = 'https://script.google.com/macros/s/AKfycbxg8K7Kc_rPuBJsLebkMpjkhmm2ePiYKVJsLbT85r9hAQQY2NCahH8jaSTRZzKvR-KLSA/exec';
 
 // [노션 연동] 데이터베이스 ID (Apps Script에서 사용, 토큰은 Apps Script에만 저장)
 const NOTION_DATABASE_IDS = {
